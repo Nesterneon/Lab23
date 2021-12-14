7a4c5537564b21543343cf532bd35bbf8a47787e
79064 <79064@MSI>
2021-12-15 00:10:03 +0300

    Добавьте файлы проекта.

---
+++ Lab23/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab23
+{
+    class Program
+    {
+        static void Factorial()
+        {
+            int result = 1;
+            for (int i = 1; i <= 11; i++)
+            {
+                result *= i;
+            }
+            Thread.Sleep(8000);
+            Console.WriteLine($"Факториал равен {result}");
+        }
+
+        static async void FactorialAsync()
+        {
+            Console.WriteLine("Начало метода FactorialAsync"); 
+            await Task.Run(() => Factorial());                
+            Console.WriteLine("Конец метода FactorialAsync");
+        }
+
+        static void Main(string[] args)
+        {
+            FactorialAsync();   
+
+            Console.WriteLine("Введите число: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Квадрат числа равен {n * n}");
+
+            Console.Read();
+        }
+    }
+}
